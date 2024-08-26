8806014a532ff10ff000b2f906f068526d64a3f0
Ajit Kumar <115556587+ajit3643@users.noreply.github.com>
2024-08-26 10:18:13 +0000

    Updated

--- Project - 1/script.js
+++ Project - 1/script.js
@@ -1,23 +1,23 @@
 
-const button = document.querySelectorAll('.box')
-const body = document.querySelector("body")
+const button = document.querySelectorAll('.box');
+const body = document.querySelector("body");
 
 button.forEach(function(box){
     box.addEventListener('click', function(color){
         if(color.target.id === 'box1'){
-            body.style.backgroundColor = "#F3B95F"
+            body.style.backgroundColor = "#F3B95F";
         }
         if(color.target.id === 'box2'){
-            body.style.backgroundColor = "#37B5B6"
+            body.style.backgroundColor = "#37B5B6";
         }
         if(color.target.id === 'box3'){
-            body.style.backgroundColor = "#50623A"
+            body.style.backgroundColor = "#50623A";
         }
         if(color.target.id === 'box4'){
-            body.style.backgroundColor = "#E6A4B4"
+            body.style.backgroundColor = "#E6A4B4";
         }
         if(color.target.id === 'box5'){
-            body.style.backgroundColor ="#D24545"
+            body.style.backgroundColor ="#D24545";
         }
         
     })
